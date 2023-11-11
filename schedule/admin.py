0358691efb9f945f2563aa4b42349ac94d184cd8
HomeSani <dfghjjhgfd311@gmail.com>
2023-11-11 20:07:11 +0300

    Add Talon model, remove user field for Cell model; add ignored rules for pydoc

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,15 +1,26 @@
 from django.contrib import admin
 
-from schedule.models import Cell, Day, Settings
+from schedule.models import Cell, Day, Settings, Talon
 
 
 @admin.register(Cell)
-class Admin(admin.ModelAdmin):
+class CellAdminModel(admin.ModelAdmin):
+    """Admin model for cell"""
+
+    list_display = ('data', 'time_start', 'time_end')
+
+
+@admin.register(Talon)
+class TalonAdminModel(admin.ModelAdmin):
+    """Admin model for talon"""
+
     list_display = ('user', 'data', 'time_start', 'time_end')
 
 
 @admin.register(Day)
-class Admin(admin.ModelAdmin):
+class DayAdminModel(admin.ModelAdmin):
+    """Admin model for day"""
+
     list_display = ('name', 'worktime_start', 'worktime_end')
 
 
--- schedule/models.py
+++ schedule/models.py
@@ -7,32 +7,64 @@ from django.db import models
 class Cell(models.Model):
     """Model for cell"""
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь, занявший время')
     data = models.DateField(auto_now_add=True, verbose_name='Дата дня для стирки')
     time_start = models.TimeField(verbose_name='Начало стирки')
     time_end = models.TimeField(verbose_name='Окончание стирки')
 
     class Meta:
+        """Meta class for model"""
+
         verbose_name = 'Ячейка'
         verbose_name_plural = 'Ячейки'
 
     def __str__(self) -> str:
+        """Return info, when called(in admin panel)"""
+        return f'Cell({self.data}): {self.time_start} - {self.time_end}'
+
+
+class Talon(models.Model):
+    """Model for talon"""
+
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name='Пользователь, занявший время'
+    )
+    data = models.DateField(auto_now_add=True, verbose_name='Дата дня для стирки')
+    time_start = models.TimeField(verbose_name='Начало стирки')
+    time_end = models.TimeField(verbose_name='Окончание стирки')
+
+    class Meta:
+        """Meta class for model"""
+
+        verbose_name = 'Талон'
+        verbose_name_plural = 'Талоны'
+
+    def __str__(self) -> str:
+        """Return info, when called(in admin panel)"""
         return f'Cell({self.user} - {self.data}): {self.time_start} - {self.time_end}'
 
 
 class Day(models.Model):
     """Model for weekday"""
 
-    index = models.PositiveIntegerField(unique=True, verbose_name='Индекс дня недели(0 - 6)')
+    index = models.PositiveIntegerField(
+        unique=True, verbose_name='Индекс дня недели(0 - 6)'
+    )
     name = models.CharField(max_length=32, verbose_name='Название дня недели')
-    worktime_start = models.TimeField(default=time(hour=6, minute=0), verbose_name='Начало работы стирки')
-    worktime_end = models.TimeField(default=time(hour=23, minute=0), verbose_name='Окончание работы стирки')
+    worktime_start = models.TimeField(
+        default=time(hour=6, minute=0), verbose_name='Начало работы стирки'
+    )
+    worktime_end = models.TimeField(
+        default=time(hour=23, minute=0), verbose_name='Окончание работы стирки'
+    )
 
     class Meta:
+        """Meta class for model"""
+
         verbose_name = 'День'
         verbose_name_plural = 'Дни'
 
     def __str__(self) -> str:
+        """Return info, when called(in admin panel)"""
         return f"{self.name}: {self.worktime_start} - {self.worktime_end}"
 
 
@@ -40,11 +72,16 @@ class Settings(models.Model):
     """Model for schedule settings"""
 
     available_days = models.ManyToManyField(Day, verbose_name='Дни недели для стирки')
-    wash_duration = models.PositiveIntegerField(default=1, verbose_name='Время стирки(одной) в часах')
+    wash_duration = models.PositiveIntegerField(
+        default=1, verbose_name='Время стирки(одной) в часах'
+    )
 
     class Meta:
+        """Meta class for model"""
+
         verbose_name = 'Настройки'
         verbose_name_plural = 'Настройки'
 
     def __str__(self) -> str:
+        """Return info, when called(in admin panel)"""
         return 'Настройки'
