0c8e8a5ced8c255cea892a779db8f96949306b16
HomeSani <dfghjjhgfd311@gmail.com>
2024-01-03 19:03:36 +0300

    Add new app(schedule)

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,20 +1,3 @@
 from django.contrib import admin
 
-from schedule.models import Day, Settings, Talon
-
-
-@admin.register(Talon)
-class TalonAdminModel(admin.ModelAdmin):
-    """Admin model for talon"""
-
-    list_display = ('user', 'date', 'time_start', 'time_end')
-
-
-@admin.register(Day)
-class DayAdminModel(admin.ModelAdmin):
-    """Admin model for day"""
-
-    list_display = ('name', 'worktime_start', 'worktime_end')
-
-
-admin.site.register(Settings)
+# Register your models here.
--- schedule/urls.py
+++ schedule/urls.py
@@ -1,7 +1,3 @@
 from django.urls import path
 
-from schedule.views import MainView
-
-urlpatterns = [
-    path('', MainView.as_view(), name='main_schedule'),
-]
+urlpatterns = []
