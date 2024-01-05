40f412c14c5d18a9b4eb5c265a95399067d60021
HomeSani <dfghjjhgfd311@gmail.com>
2024-01-05 12:48:02 +0300

    Add models in admin panel

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from schedule.models import Cell, Day, Settings
+
+
+@admin.register(Day)
+class DayAdmin(admin.ModelAdmin):
+    list_display = (
+        'index',
+        'start_time',
+        'end_time',
+    )
+
+
+@admin.register(Cell)
+class CellAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'start_time',
+        'end_time',
+        'date',
+        'is_occupied',
+    )
+    list_editable = ('is_occupied',)
+    date_hierarchy = 'date'
+    list_filter = ('is_occupied',)
+
+
+@admin.register(Settings)
+class SettingsAdmin(admin.ModelAdmin):
+    pass
