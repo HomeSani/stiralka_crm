5f92f32df440dc8948fd26cc6a4019dc02e247cd
HomeSani <dfghjjhgfd311@gmail.com>
2024-01-06 20:13:41 +0300

    Update env variables

--- project_root/settings.py
+++ project_root/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 AUTH_USER_MODEL = 'users.User'
 
@@ -72,8 +72,11 @@ WSGI_APPLICATION = 'project_root.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASS'),
+        'HOST': 'db',
     }
 }
 
